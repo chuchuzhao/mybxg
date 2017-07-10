b06ac61244904ef83a6875a57160d7c6e54ba578
chuchuzhao <1948668217@qq.com>
2017-07-10 18:16:28 +0800

    实现讲师预览功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,17 +1,40 @@
-define(['jquery','template','bootstrap'],function($,template){
+define(['jquery', 'template', 'bootstrap'], function ($, template) {
     /*
     讲师管理
     */
     //调用接口，获取数据，渲染界面
     $.ajax({
-        type:'get',
-        url:'/api/teacher',
-        dataType:'json',
-        success:function(data){
+        type: 'get',
+        url: '/api/teacher',
+        dataType: 'json',
+        success: function (data) {
             // console.log(data);
-            var html=template('teacherTpl',{list:data.result});
-            console.log(html);
+            var html = template('teacherTpl', {
+                list: data.result
+            });
+            // console.log(html);
             $('#teacherInfo').html(html);
+            //实现讲师信息预览
+            $('#teacherInfo').find('.preview').click(function () {
+                var td = $(this).closest('td'); //获取相邻父元素
+                var tcId = td.attr('data-tcId');
+                //获取详细信息
+                $.ajax({
+                    type: 'get',
+                    url: '/api/teacher/view',
+                    data: {
+                        tc_id: tcId
+                    },
+                    dataType: 'json',
+                    success: function (data) {
+                        var html = template('modalTpl', data.result);
+                        // console.log(html);
+                        $('#modalInfo').html(html);
+                        $('#teacherModal').modal();
+                    }
+
+                });
+            })
         }
-    })
+    });
 });
