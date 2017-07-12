155aaa3bda73f5c260a401e761edbc9f2034c530
chuchuzhao <1948668217@qq.com>
2017-07-12 20:59:40 +0800

    完成个人中心表单提交

--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,4 @@
-define(['jquery', 'template', 'util', 'editor', 'uploadify', 'region'], function ($, template, util, CKEDITOR) {
+define(['jquery', 'template', 'util', 'editor', 'uploadify', 'region', 'datepicker', 'language', 'validate', 'form'], function ($, template, util, CKEDITOR) {
     util.setMenu('/index/index');
     $.ajax({
         type: 'get',
@@ -44,6 +44,37 @@ define(['jquery', 'template', 'util', 'editor', 'uploadify', 'region'], function
                         groups: ['about']
                     }
                 ]
+            });
+            //处理表单验证和提交
+            $('#settingsForm').validate({
+                sendForm: false,
+                valid: function () {
+                    //处理富文本内容更新
+                    for (var instance in CKEDITOR.instances) {
+                        CKEDITOR.instances[instance].updateElement();
+                    }
+                    //处理籍贯信息
+                    var p = $('#p option:selected').text();
+                    var c = $('#c option:selected').text();
+                    var d = $('#d option:selected').text();
+                    var hometown = p + '|' + c + '|' + d;
+                    //处理表单提交
+                    $(this).ajaxSubmit({
+                        type: 'post',
+                        url: '/api/teacher/modify',
+                        data: {
+                            tc_hometown: hometown
+                        },
+                        dataType: 'json',
+                        success: function (data) {
+                            // console.log(data);
+                            if (data.code == 200) {
+                                console.log($(this));
+                                location.reload();
+                            }
+                        }
+                    });
+                }
             });
         }
     });
